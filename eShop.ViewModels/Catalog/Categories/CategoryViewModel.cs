13b3402cb11f4c1d66b26d5469634dfbf69d77d1
cuong012qn <cuong012qn121@gmail.com>
2021-06-04 00:41:05 +0700

    ViewModels

---
+++ eShop.ViewModels/Catalog/Categories/CategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.ViewModels.Catalog.Categories
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Link { get; set; }
+    }
+}
