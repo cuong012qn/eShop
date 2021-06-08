f92af1926034b33d63959109436d92a919db3920
cuong012qn <cuong012qn121@gmail.com>
2021-06-09 00:06:53 +0700

    edit query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,9 @@
 select * from Category
 select * from Product
 select * from ProductImage
+
+select Category.Id, Category.Name, COUNT(Category.Name), Product.Name from Product 
+join ProductCategory on Product.Id = ProductCategory.ProductId
+join Category on Category.Id = ProductCategory.CategoryId
+group by Category.Id
+
